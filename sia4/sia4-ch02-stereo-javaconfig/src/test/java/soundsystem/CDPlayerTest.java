70cdfb47ce824139517076dd7ab2a7a45b228844
vision8 tech <vision8r@gmail.com>
2017-11-09 17:13:19 +0200

    ch 02, javaconfig

--- sia4/sia4-ch02-stereo-javaconfig/src/test/java/soundsystem/CDPlayerTest.java
+++ sia4/sia4-ch02-stereo-javaconfig/src/test/java/soundsystem/CDPlayerTest.java
@@ -10,37 +10,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = CDPlayerConfig.class)
+@ContextConfiguration(classes=CDPlayerConfig.class)
 public class CDPlayerTest {
-	
-	@Rule
-	public final StandardOutputStreamLog log = new StandardOutputStreamLog();
-	
-	@Autowired
-	private MediaPlayer player;
-	
-	@Autowired
-	private CompactDisc cd;
-	
-	
-	@Test
-	public void cdShouldNotBeNull() {
-		
-		assertNotNull(cd);
-		
-	}
-	
-	
-	@Test
-	public void play() {
-		
-		player.play();
-		assertEquals(
-				"Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\n",
-				log.getLog());
-		
-	}
-	
+
+  @Rule
+  public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
+  @Autowired
+  private MediaPlayer player;
+
+  @Test
+  public void play() {
+    player.play();
+    assertEquals(
+        "Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\n", 
+        log.getLog());
+  }
+
 }
