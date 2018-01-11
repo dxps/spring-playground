b5eb66c3110717134ded050ad6e171afdfa91453
vision8 tech <vision8r@gmail.com>
2018-01-11 23:27:45 +0200

    (sia5) up to 2.3

---
+++ sia5/taco-cloud/src/main/java/tacos/Design.java
@@ -0,0 +1,22 @@
+package tacos;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+/**
+ * @author vision8
+ */
+@Data
+public class Design {
+	
+	@NotNull
+	@Size(min=5, message = "Name must be at least 5 characters long")
+	private String name;
+	
+	@Size(min=1, message = "You must choose at least one ingredient")
+	private List<String> ingredients;
+	
+}
---
+++ sia5/taco-cloud/src/main/java/tacos/OrderController.java
@@ -0,0 +1,38 @@
+package tacos;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * @author vision8
+ */
+@Controller
+@RequestMapping("/orders")
+@Slf4j
+public class OrderController {
+	
+	
+	@GetMapping("/current")
+	public String orderForm(Model model) {
+		
+		model.addAttribute("order", new Order());
+		return "orderForm";
+		
+	}
+	
+	
+	@PostMapping
+	public String processOrder(Order order) {
+		
+		log.info("Order submitted: " + order);
+		return "redirect:/";
+	
+	}
+	
+	
+}
