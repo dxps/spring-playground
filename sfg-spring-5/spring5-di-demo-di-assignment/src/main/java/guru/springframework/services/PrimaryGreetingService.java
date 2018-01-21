e25e00aa72ae8f9c7faf5f3a491ff0e618e9e599
vision8 tech <vision8r@gmail.com>
2018-01-21 23:43:42 +0200

    sfg-spring5, section 3, lecture 38: DI Assignment done (incl other GreetingService impl)

--- sfg-spring-5/spring5-di-demo-di-assignment/src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ sfg-spring-5/spring5-di-demo-di-assignment/src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -13,7 +13,11 @@ import org.springframework.stereotype.Service;
 public class PrimaryGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
-
+    
+    public PrimaryGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+    
     @Override
     public String sayGreeting() {
         return greetingRepository.getEnglishGreeting();
--- sfg-spring-5/spring5-di-demo-di-assignment/src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ sfg-spring-5/spring5-di-demo-di-assignment/src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -13,9 +13,14 @@ import org.springframework.stereotype.Service;
 public class PrimarySpanishGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
-
+    
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+    
     @Override
     public String sayGreeting() {
         return greetingRepository.getSpanishGreeting();
     }
+    
 }
