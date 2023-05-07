cc55990a5bb14fbe1d01dbf66c18ebec494ed2a6
dxps <marius.ileana@gmail.com>
2023-05-07 20:47:04 +0300

    chore: sb3-kb-refresh Rename and @GetMapping, instead of @RequestMapping

--- sb3-kb-refresh/src/main/java/io/dxps/springboot/sb3refreshkb/ex1/CourseController.java
+++ sb3-kb-refresh/src/main/java/io/dxps/springboot/sb3refreshkb/ex1/CourseController.java
@@ -1,21 +1,20 @@
 package io.dxps.springboot.sb3refreshkb.ex1;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
 
-    @RequestMapping("/courses")
+    @GetMapping("/courses")
     List<Course> getCourses() {
         return Arrays.asList(
                 new Course("1", "First course", "1st course's description"),
                 new Course("2", "Second course", "2nd course's description"),
-                new Course("3", "Third course", "3rd course's description")
-        );
+                new Course("3", "Third course", "3rd course's description"));
     }
 
 }
